09de89a1ce975bca4463423667d2e100c8e33d35
Jeanie Ng <ng.jeanie021@gmail.com>
2020-08-22 16:27:49 -0400

    Setup status update UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import Background from './office.svg'
+import PermanentDrawerRight from './drawer.jsx'
 
-const App = () => <div></div>;
+const App = () => {
+  return (
+    <>
+    <div style={{
+      backgroundImage: `url(${Background})`,
+      backgroundRepeat: 'no-repeat',
+      height: '100vh',
+      width: '100vw',
+      marginTop: '5%'
+      }}>
+    </div>
+    <PermanentDrawerRight />
+    </>
+  );
+}
 
 export default App;
