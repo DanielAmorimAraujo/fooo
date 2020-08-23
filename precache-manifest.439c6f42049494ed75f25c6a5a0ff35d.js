21ae11f29367033970bd2575effdf799221c7fef
Daniel Araujo <daniel.amorim.araujo@gmail.com>
2020-08-23 10:15:59 -0400

    Updates

---
+++ precache-manifest.439c6f42049494ed75f25c6a5a0ff35d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee2af5bc08e726c3f29ef349b488e1d",
+    "url": "/fooo/index.html"
+  },
+  {
+    "revision": "d2a508f82c18d844d78f",
+    "url": "/fooo/static/js/2.cda9b6a9.chunk.js"
+  },
+  {
+    "revision": "9e85ddb571623d0af0d22cc3c513cdac",
+    "url": "/fooo/static/js/2.cda9b6a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d51b20cdbe01c4107dd",
+    "url": "/fooo/static/js/main.bc8a1359.chunk.js"
+  },
+  {
+    "revision": "db3eea79fef7f6874c99",
+    "url": "/fooo/static/js/runtime-main.1483d7f3.js"
+  },
+  {
+    "revision": "231684e0c1504a0c956113dbfda71922",
+    "url": "/fooo/static/media/bam.231684e0.png"
+  },
+  {
+    "revision": "3ccbe908431439c056aae686ce2b8ecd",
+    "url": "/fooo/static/media/beau.3ccbe908.png"
+  },
+  {
+    "revision": "7fb405684c229da099786750857f3baa",
+    "url": "/fooo/static/media/chair-2.7fb40568.png"
+  },
+  {
+    "revision": "04cad58b1deddd1b32e2e7d2754120c2",
+    "url": "/fooo/static/media/chair.04cad58b.png"
+  },
+  {
+    "revision": "a97f30e15f669b0e839e26405c5380f5",
+    "url": "/fooo/static/media/marina.a97f30e1.png"
+  },
+  {
+    "revision": "6785c2a8fa807d3e9db13682cf54ec5b",
+    "url": "/fooo/static/media/nook.6785c2a8.png"
+  },
+  {
+    "revision": "fe6ceb5721052dbd9d31b39ecac83ba9",
+    "url": "/fooo/static/media/office.fe6ceb57.svg"
+  },
+  {
+    "revision": "52f1aca71ca65ae1190d197008af421e",
+    "url": "/fooo/static/media/table-2.52f1aca7.png"
+  },
+  {
+    "revision": "f0d65e664066ce94ef29630604a9ab13",
+    "url": "/fooo/static/media/table.f0d65e66.png"
+  },
+  {
+    "revision": "3233742e0b51b58ae94ceb236355dea9",
+    "url": "/fooo/static/media/tia.3233742e.png"
+  }
+]);
